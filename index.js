5a99c20112620887c0abf35de5e3d8e9f948e670
AritraApon <aritromazumdar8@gmail.com>
2026-05-19 12:27:13 +0600

    all tutors api make

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
-const port = process.env.PORT
 dotenv.config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const uri = process.env.MONGO_URI;
+const port = process.env.PORT
 
 app.use(express.json())
 app.use(cors())
@@ -21,7 +21,38 @@ const client = new MongoClient(uri, {
 
 
 async function run() {
+
   try {
+ //Collections
+   const db = client.db('mediQueueDB');
+   const tutorCollection = db.collection('tutors')
+
+
+//    Add tutor POST
+
+   app.post('/tutors' , async(req ,res) =>{
+    const newTutor = req.body;
+   console.log(newTutor)
+    const result = await  tutorCollection.insertOne(newTutor)
+    res.send(result)
+
+   }  )
+
+//get post
+app.get('/tutors',async (req, res) => {
+  const cursor = tutorCollection.find()
+  const result = await cursor.toArray();
+  res.send(result);
+});
+
+// app.get('/my-tutors', async (req, res) => {
+//     const email = req.query.email;
+//     const query = { userEmail: email }; 
+//     const result = await tutorCollection.find(query).toArray();
+//     res.send(result);
+// });
+
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
